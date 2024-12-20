4fd3aa3909a95906e993ce376022ffa43b885043
IrfanHidayatulahP <irfanhputra400@gmail.com>
2024-12-20 20:53:16 +0700

    Membuat InsertBodyMK yang didalam nya ada FormMataKuliah

--- app/src/main/java/com/example/ucp2/ui/view/MatKul/InsertMKView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/MatKul/InsertMKView.kt
@@ -1,10 +1,12 @@
 package com.example.ucp2.ui.view.MatKul
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -18,6 +20,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
@@ -26,6 +29,36 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2.ui.viewmodel.MataKuliah.FormErrorState
 import com.example.ucp2.ui.viewmodel.MataKuliah.InsertMKViewModel
 import com.example.ucp2.ui.viewmodel.MataKuliah.MKEvent
+import com.example.ucp2.ui.viewmodel.MataKuliah.MKUIState
+
+
+@Composable
+fun InsertBodyMK(
+    modifier: Modifier = Modifier,
+    onValueChange: (MKEvent) -> Unit,
+    uiState: MKUIState,
+    onClick: () -> Unit
+) {
+    Column (
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        FormMataKuliah(
+            mkEvent = uiState.MatkulEvent,
+            onValueChange = onValueChange,
+            errorState = uiState.isEntryValid,
+            onValueChangedEvent = { selectedDosen ->
+                onValueChange(uiState.MatkulEvent.copy(dosenPengampu = selectedDosen))
+            },
+            label = "Pilih Dosen",
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(onClick = onClick) {
+            Text("Simpan")
+        }
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
